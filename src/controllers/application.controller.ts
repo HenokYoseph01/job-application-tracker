6ca4b579aaab2701291df1f83b8a63e2cc6ebb87
HenokYoseph01 <henok.y2014@gmail.com>
2026-05-18 09:56:48 +0300

    Inital application route and controllers

---
+++ src/controllers/application.controller.ts
@@ -0,0 +1,21 @@
+import {type Request, type Response} from "express";
+
+
+const getAllApplications = (req: Request, res: Response) => {}
+
+const createApplication = (req: Request, res: Response) => {}
+
+const getApplicationById = (req: Request, res: Response) => {}
+
+const updateApplication = (req: Request, res: Response) => {}
+
+const deleteApplication = (req: Request, res: Response) => {}
+
+
+export default {    
+    getAllApplications,
+    createApplication,
+    getApplicationById,
+    updateApplication,
+    deleteApplication
+}
---
+++ src/routes/application.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import applicationController from "../controllers/application.controller.js";
+
+//Create a router for the application routes
+const router = Router();
+
+router.get("/", applicationController.getAllApplications);
+router.post("/", applicationController.createApplication);
+router.get("/:id", applicationController.getApplicationById);
+router.patch("/:id", applicationController.updateApplication);
+router.delete("/:id", applicationController.deleteApplication);   
