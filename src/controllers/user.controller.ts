21df0192eb9dc239b6e75a49ea583617b6accfe3
HenokYoseph01 <henok.y2014@gmail.com>
2026-05-20 09:08:57 +0300

    Set inital controller logic for user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,7 @@
+import {type Request, type Response} from 'express';
+
+const registerUser = async(req: Request, res: Response) => {}
+
+const loginUser = async(req: Request, res: Response) => {}
+
+const getUserProfile = async(req: Request, res: Response) => {}
